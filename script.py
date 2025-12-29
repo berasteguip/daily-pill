be7f3e6574c6167507fc77caf2e8394f8261c33f
Pablo <202311460@alu.comillas.edu>
2025-12-29 20:51:38 +0100

    asdfa

--- script.py
+++ script.py
@@ -1,8 +1,20 @@
 from datetime import datetime, timezone
 
+days = {
+    'Monday': 'art_culture',
+    'Tuesday': 'geography', 
+    'Wednesday': 'history', 
+    'Thursday': 'nature', 
+    'Friday': 'politics_econ', 
+    'Saturday': 'science_tech',
+    'Sunday': 'sayings'
+}
+
 def main():
-    now = datetime.now(timezone.utc).isoformat()
-    string = f"UTC now: {now}"
+    now = datetime.now(timezone.utc)
+    day = now.strftime("%A")
+    topic = days[day]
+    string = f"UTC now: {now}, Topic: {topic}"
     print(string)
     with open("history.txt", "a") as f:
         f.write(string + "\n")
